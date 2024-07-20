042f751b6941f894b77dbfe98de83cc52322ba4a
Alok Swaminathan <swaminathanalok@gmail.com>
2024-07-20 14:05:34 -0400

    Remove unnecessary function

--- tools/fw_update.py
+++ tools/fw_update.py
@@ -46,33 +46,6 @@ RESP_ERROR = b"\x03"
 FRAME_SIZE = 256
 
 
-def send_metadata(ser, metadata, debug=False):
-    assert (len(metadata) == 4)
-    version = u16(metadata[:2], endian='little')
-    size = u16(metadata[2:], endian='little')
-    print(f"Version: {version}\nSize: {size} bytes\n")
-
-    # Handshake for update
-    ser.write(b"U")
-
-    print("Waiting for bootloader to enter update mode...")
-    while ser.read(1).decode() != "U":
-        print("got a byte")
-        pass
-
-    # Send size and version to bootloader.
-    if debug:
-        print(metadata)
-
-    ser.write(metadata)
-
-    # Wait for an OK from the bootloader.
-    resp = ser.read(1)
-    if resp != RESP_OK:
-        raise RuntimeError(
-            "ERROR: Bootloader responded with {}".format(repr(resp)))
-
-
 def send_frame(ser, frame, debug=False):
     ser.write(frame)  # Write the frame...
 
@@ -98,12 +71,6 @@ def send_frame(ser, frame, debug=False):
 def update(ser, infile, debug):
     # Open serial port. Set baudrate to 115200. Set timeout to 2 seconds.
     with open(infile, "rb") as fp:
-        firmware_blob = fp.read()
-
-    metadata = firmware_blob[:4]
-    firmware = firmware_blob[4:]
-
-    send_metadata(ser, metadata, debug=debug)
 
     for idx, frame_start in enumerate(range(0, len(firmware), FRAME_SIZE)):
         data = firmware[frame_start: frame_start + FRAME_SIZE]
@@ -113,6 +80,7 @@ def update(ser, infile, debug):
 
         send_frame(ser, frame, debug=debug)
         print(f"Wrote frame {idx} ({len(frame)} bytes)")
+        firmware = fp.read()
 
     print("Done writing firmware.")
 
