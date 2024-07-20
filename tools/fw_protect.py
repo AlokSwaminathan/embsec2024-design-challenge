185516aacc30bfb6cd1a64e95b87b98666020d75
Alok Swaminathan <swaminathanalok@gmail.com>
2024-07-20 00:48:54 -0400

    Format

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -16,43 +16,44 @@ from Crypto.PublicKey import ECC
 from Crypto.Signature import eddsa
 from Crypto.Hash import HMAC, SHA512
 import base64
-        
 
 
-def protect_firmware(infile: str, outfile: str, version: int, message: str,secret_file: str):
+def protect_firmware(infile: str, outfile: str, version: int, message: str, secret_file: str):
     # Load firmware binary from infile
     with open(infile, mode="rb") as fp:
         firmware = fp.read()
-    
+
     # Read secrets as a json file
     with open(secret_file, mode="r") as fp:
         secrets = json.load(fp)
 
     # Pack version and size into two little-endian shorts
-    metadata = p16(version, endian='little') + p16(len(firmware), endian='little')  
+    metadata = p16(version, endian='little') + \
+        p16(len(firmware), endian='little')
 
     # Combine parts into single firmware blob
     firmware_blob = metadata + firmware + message.encode() + b"\x00"
-    
+
     ed25519_private_key = base64.b64decode(secrets["ed25519_private_key"])
     ed25519_private_key = ECC.import_key(ed25519_private_key)
-    signer = eddsa.new(ed25519_private_key,mode='rfc8032')
+    signer = eddsa.new(ed25519_private_key, mode='rfc8032')
     signature = signer.sign(firmware_blob)
     signed_firmware_blob = firmware_blob + signature
-    
+
     hmac_key = base64.b64decode(secrets["hmac_key"])
     hmac = HMAC.new(hmac_key, digestmod=SHA512)
     hmac.update(signed_firmware_blob)
     hmac_digest = hmac.digest()
-    
+
     signed_hashed_firmware_blob = signed_firmware_blob + hmac_digest
-    
+
     aes_key = base64.b64decode(secrets["aes_key"])
     aes_nonce = os.urandom(16)
-    
+
     aes = AES.new(aes_key, AES.MODE_GCM, nonce=aes_nonce)
-    aes_ciphertext, aes_tag = aes.encrypt_and_digest(signed_hashed_firmware_blob)
-    
+    aes_ciphertext, aes_tag = aes.encrypt_and_digest(
+        signed_hashed_firmware_blob)
+
     final_firmware_blob = aes_nonce + aes_ciphertext + aes_tag
 
     # Write firmware blob to outfile
@@ -62,11 +63,17 @@ def protect_firmware(infile: str, outfile: str, version: int, message: str,secre
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Firmware Update Tool")
-    parser.add_argument("--infile", help="Path to the firmware image to protect.", required=True)
-    parser.add_argument("--outfile", help="Filename for the output firmware.", required=True)
-    parser.add_argument("--version", help="Version number of this firmware.", required=True,type=int)
-    parser.add_argument("--message", help="Release message for this firmware.", required=True)
-    parser.add_argument("--secrets", help="Path to the secrets json file.", required=True)
+    parser.add_argument(
+        "--infile", help="Path to the firmware image to protect.", required=True)
+    parser.add_argument(
+        "--outfile", help="Filename for the output firmware.", required=True)
+    parser.add_argument(
+        "--version", help="Version number of this firmware.", required=True, type=int)
+    parser.add_argument(
+        "--message", help="Release message for this firmware.", required=True)
+    parser.add_argument(
+        "--secrets", help="Path to the secrets json file.", required=True)
     args = parser.parse_args()
 
-    protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message,secret_file=args.secrets)
+    protect_firmware(infile=args.infile, outfile=args.outfile, version=int(
+        args.version), message=args.message, secret_file=args.secrets)
