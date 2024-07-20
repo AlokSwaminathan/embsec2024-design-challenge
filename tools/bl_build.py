4beb162a03cc6866ded7ba08df8d87e1e8940e77
Alok Swaminathan <swaminathanalok@gmail.com>
2024-07-20 16:18:02 -0400

    Add key and secrets file generation to bl_build.py

--- tools/bl_build.py
+++ tools/bl_build.py
@@ -12,12 +12,15 @@ the build outputs into the host tools directory for programming.
 import os
 import pathlib
 import subprocess
+import json
+from Crypto.PublicKey import ECC
+import base64
 
 REPO_ROOT = pathlib.Path(__file__).parent.parent.absolute()
 BOOTLOADER_DIR = os.path.join(REPO_ROOT, "bootloader")
 
 
-def make_bootloader() -> bool:
+def make_bootloader(ed25519_pub_key, aes_key, hmac_key) -> bool:
     # Build the bootloader from source.
 
     os.chdir(BOOTLOADER_DIR)
@@ -28,6 +31,24 @@ def make_bootloader() -> bool:
     # Return True if make returned 0, otherwise return False.
     return status == 0
 
+def save_to_secrets(ed25519_private_key, aes_key, hmac_key):
+    json_data = {
+        "ed25519_private_key": base64.b64encode(ed25519_private_key).decode('utf-8'),
+        "aes_key": base64.b64encode(aes_key).decode('utf-8'),
+        "hmac_key": base64.b64encode(hmac_key).decode('utf-8')
+    }
+    os.chdir(BOOTLOADER_DIR)
+    with open("secret_build_outputs.txt", "w") as f:
+        json.dumps(json_data, f,indent=4)
 
 if __name__ == "__main__":
-    make_bootloader()
+    ed25519_key = ECC.generate(curve='ed25519')
+    ed25519_private_key = ed25519_key.export_key(format='PEM')
+    ed25519_public_key = ed25519_key.public_key().export_key(format='PEM')
+    aes_key = os.urandom(32)
+    hmac_key = os.urandom(32)
+    if make_bootloader(ed25519_public_key,aes_key,hmac_key):
+        save_to_secrets(ed25519_private_key,aes_key,hmac_key)
+    else:
+        print("Failed to build bootloader.")
+        exit(1)
