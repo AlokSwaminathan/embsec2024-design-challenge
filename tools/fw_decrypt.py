d576395341283948886d0067ea7af17ad5ef8300
Alok Swaminathan <swaminathanalok@gmail.com>
2024-07-22 19:13:10 -0400

    Remove HMAC from fw_decrypt

--- tools/fw_decrypt.py
+++ tools/fw_decrypt.py
@@ -14,6 +14,7 @@ from Crypto.Cipher import AES
 from Crypto.PublicKey import ECC
 from Crypto.Signature import eddsa
 from Crypto.Hash import HMAC, SHA512
+from Crypto.Util.Padding import unpad
 import base64
 
 
@@ -28,37 +29,21 @@ def decrypt_firmware(infile: str, outfile: str, secret_file: str):
     #Extract AES IV, ciphertext, and tag from infile 
     aes_iv = protected_firmware[:16]
     aes_ciphertext = protected_firmware[16:-16]
-    aes_tag = protected_firmware[-16:]
     aes_key = base64.b64decode(secrets["aes_key"])
 
     #Initiliaze AES key in GCM mode
-    aes = AES.new(aes_key, AES.MODE_GCM, nonce=aes_iv) 
+    aes = AES.new(aes_key, AES.MODE_CBC, nonce=aes_iv) 
 
     try:
-        firmware_blob = aes.decrypt_and_verify(aes_ciphertext, aes_tag)
+        signed_firmware_blob = unpad(aes.decrypt(aes_ciphertext),AES.block_size)
         print("AES decryption successful.")
     except ValueError:
         print("Decryption failed. Check the AES key.")
         return
     
-    #Extract and verify the HMAC
-    hmac_key = base64.b64decode(secrets["hmac_key"])
-    prehash = firmware_blob[:-64]
-    hmac_digest = firmware_blob[-64:]
-    
-    hmac = HMAC.new(hmac_key, digestmod=SHA512)
-    hmac.update(prehash)
-    
-    try:
-        hmac.verify(hmac_digest)
-        print("HMAC verification passed.")
-    except ValueError:
-        print("HMAC verification failed. Check the HMAC key.")
-        return
-    
     #Extract signature and firmware and verify through ed25519 verification 
-    firmware_blob = prehash[:-64]
-    signature = prehash[-64:]
+    firmware_blob = signed_firmware_blob[:-64]
+    signature = signed_firmware_blob[-64:]
     ed25519_public_key = ECC.import_key(base64.b64decode(secrets["ed25519_public_key"]))
     verifier = eddsa.new(ed25519_public_key, mode='rfc8032')
     
@@ -72,11 +57,11 @@ def decrypt_firmware(infile: str, outfile: str, secret_file: str):
     version = u16(firmware_blob[:2], endian='little')
     size = u16(firmware_blob[2:4], endian='little')
     firmware = firmware_blob[4:size+4]
-    release_message = firmware_blob[size+4:-1].decode('utf-8')
+    release_message = firmware_blob[size+4:-1].decode('ascii')
     
     # Write decrypted firmware into output file
-    with open(outfile, mode="wb+") as fp:
-        fp.write(firmware)
+    with open(outfile, mode="wb+") as decrypted_binary:
+        decrypted_binary.write(firmware)
     
     print(f"Version: {version}\nFirmware Size: {size} bytes\nRelease Message: {release_message}")
     
