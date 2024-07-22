b3450d705439e0a85a7e4aa3a1da562f0e06bf44
Alok Swaminathan <swaminathanalok@gmail.com>
2024-07-22 19:59:58 -0400

    Fix bl_build

--- tools/bl_build.py
+++ tools/bl_build.py
@@ -21,9 +21,9 @@ REPO_ROOT = pathlib.Path(__file__).parent.parent.absolute()
 BOOTLOADER_DIR = os.path.join(REPO_ROOT, "bootloader")
 
 
-def padded_char_array(key):
-    char_array = "{" + ", ".join([f"'{c}'" for c in key])
-    padding = ['\\0' for _ in range(0, 4 - (len(key) % 4))] if len(key) % 4 != 0 else []
+def padded_uint8t_array(key):
+    char_array = "{" + ", ".join([f"{0xb:02x}" for b in key])
+    padding = ['0x00' for _ in range(0, 4 - (len(str) % 4))] if len(str) % 4 != 0 else []
     return char_array + (", " + ", ".join(padding) + "}") if padding else char_array + "}"
 
 def make_bootloader(ed25519_pub_key, aes_key) -> bool:
@@ -31,9 +31,11 @@ def make_bootloader(ed25519_pub_key, aes_key) -> bool:
     os.chdir(BOOTLOADER_DIR)
 
     # Write the keys to a secret header file
-    with open("inc/secrets.h", "w") as f:
-        f.write("#define ED25519_PUBLIC_KEY " + padded_char_array(ed25519_pub_key) + "\n")
-        f.write("#define AES_KEY " + padded_char_array(aes_key) + "\n")
+    with open("inc/secrets.h", "w") as secrets_header:
+        secrets_header.write("#define ED25519_PUBLIC_KEY " + padded_uint8t_array(ed25519_pub_key) + "\n")
+        secrets_header.write("#define ED25519_PUBLIC_KEY_SIZE " + str(len(ed25519_pub_key)) + "\n")
+        secrets_header.write("#define AES_KEY " + padded_uint8t_array(aes_key) + "\n")
+        secrets_header.write("#define AES_KEY_SIZE " + str(len(aes_key)) + "\n")
 
     # Clean current directory to build bootloader
     subprocess.call("make clean", shell=True)
@@ -50,21 +52,22 @@ def save_to_secrets(ed25519_private_key, aes_key):
         "aes_key": aes_key
     }
     os.chdir(os.path.join(BOOTLOADER_DIR,"bin/"))
-    with open("bin/secret_build_outputs.json", "w") as f:
-        f.write(json.dump(json_data, f, indent=4))
+    with open("secret_build_outputs.json", "w") as f:
+        json.dump(json_data, f, indent=4)
 
 if __name__ == "__main__":
-    # Generate Ed25519 keys and encode in base64
+    # Generate Ed25519 keys and encode in base64 if needed
     ed25519_key = ECC.generate(curve='ed25519')
-    ed25519_private_key = base64.b64encode(ed25519_key.export_key(format='PEM')).decode('ascii')
-    ed25519_public_key = base64.b64encode(ed25519_key.public_key().export_key(format='PEM')).decode('ascii')
+    ed25519_private_key_b64 = base64.b64encode(ed25519_key.export_key(format='PEM').encode('ascii')).decode('ascii')
+    ed25519_public_key = ed25519_key.public_key().export_key(format='PEM').encode('ascii')
 
     # Generate AES key and encode in base64
-    aes_key = base64.b64encode(os.urandom(32)).decode('ascii')
+    aes_key = os.urandom(32)
+    aes_key_b64 = base64.b64encode(aes_key).decode('ascii')
 
     # If build successful, save keys to secret file
     if make_bootloader(ed25519_public_key, aes_key):
-        save_to_secrets(ed25519_private_key, aes_key)
+        save_to_secrets(ed25519_private_key_b64, aes_key_b64)
         print("Bootloader built successfully. Secrets saved.")
     else:
         print("Failed to build bootloader.")
